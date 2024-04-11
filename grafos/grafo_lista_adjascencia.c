de75ac0f0667be9359e0673954de16d5906c7144
andrew dylan viana amorim <andrewdylanvianaamorim.2@gmail.com>
2024-04-10 21:52:39 -0300

    Começando grafos

---
+++ grafos/grafo_lista_adjascencia.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <"grafo_lista_adjascencia.h">
+
+
+struct Grafo *cria_grafo(void) {
+	struct Grafo *grafo = calloc(1, sizeof(struct Grafo));
+	return grafo;	
+}
+
+struct Vertice *procura_vertice(struct Grafo *grafo,int dado) {
+	struct Vertice vertice = NULL;	
+	for(int i = 0; i < grafo->numero_vertices; i++) {
+		if(grafo->vertices[i].dado == dado) {
+			vertice = grafo->vertices + i;
+			break;
+		}
+	}
+
+	return vertice;
+}
+
+struct Vertice *cria_vertice(struct Grafo *grafo, int dado) {
+	struct Vertice *vertice = NULL;
+	int pode_criar = 1;
+	for(int i = 0; i < grafo->numero_vertices; i++) {
+		if(grafo->vertices[i].dado == dado) {
+			pode_criar = 0;
+			break;
+		}
+	}
+	
+	if(pode_criar == 0) return NULL;
+
+	vertice = calloc(1, sizeof(struct Vertice));
+
+	return vertice;
+}
+
+
+
+
+int main(void) {
+	puts("Olá, mundo!");
+	exit(0);
+}
---
+++ grafos/grafo_lista_adjascencia.h
@@ -0,0 +1,18 @@
+#ifndef GRAFO_LISTA_ADJASCENCIA
+#define GRAFO_LISTA_ADJASCENCIA
+
+struct Vertice {
+	int dado;
+	struct Vertice **adjascentes;
+};
+
+struct Grafo {
+	int numero_vertices;
+	struct Vertice *vertices;
+}
+
+struct Grafo *cria_grafo(void);
+
+struct Vertice *procura_vertice(struct Grafo *grafo,int dado);
+struct Vertice *cria_vertice(struct Grafo *grafo, int dado);
+#endif
